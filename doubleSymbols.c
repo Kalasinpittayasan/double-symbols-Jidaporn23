94688d1e1e1906b2153d5d9e0923e3c0fdb24f34
Jidaporn23 <50321@kalasinpit.ac.th>
2025-02-17 15:28:30 +0700

    Update doubleSymbols.c

--- doubleSymbols.c
+++ doubleSymbols.c
@@ -4,14 +4,11 @@ int main() {
   int n;
   scanf("%d", &n);
   if (n % 2 != 0) {
-    printf("Wrong input\n");
-    return 1;
+    printf("Wrong input");
   }
 
   for (int i = 0; i < n; i+=2) {
-    if (i % 2 == 0) {
       printf("*+");
-    }
   }
 
   return 0;
